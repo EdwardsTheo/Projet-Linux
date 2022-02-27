800cbd7a6fb93915aa60997fb716b433d9aa4f8b
EdwardsTheo <baptiste.theobald@outlook.fr>
2022-02-27 11:03:01 +0100

    fiNITO PIPO

--- script/client.sh
+++ script/client.sh
@@ -16,12 +16,13 @@ function main {
 
 function request_ip {
 	req=$1
+	echo -e "\n${Blue}Envoie de la requête DNS_ASK${Blue}"
 	request "$req"
 	var=$(cat ../out/heard.out)
 	if [[ $var == "-1" ]]; then 
-		echo -e "\nLe serveur n'a pas renvoyé d'IP car le nom n'existe pas dans la base de donnée"
+		echo -e "\n${Red}Le serveur n'a pas renvoyé d'IP car le nom n'existe pas dans la base de donnée${Red}"
 	else 
-	 	echo -e "\nLe serveur a renvoyé l'adresse IP et masque suivant :"
+	 	echo -e "\n${Green}Le serveur a renvoyé l'adresse IP et masque suivant :${Green}"
 	 	IFS=';' 
 	 	read -a strarr <<<"$var" 
 	 	echo -e "1  IP address :     ${strarr[0]} "  
@@ -32,27 +33,29 @@ function request_ip {
 function request_fin {
 	req=$1 
 	if [[ $req == "FIN" ]]; then 
+		echo -e "\n${Blue}Envoie de la requête FIN${Blue}"
 		request "$req"
-		echo -e "\nLe serveur a bien été coupé"
+		echo -e "${Green}Le serveur a bien été coupé${Green}"
 		exit
 	fi
 }
 
 function request_syn {
+	echo -e "\n${Blue}Envoie de la requête DNS_SYN${Blue}"
 	request "DNS_SYN"
 	answer=$(cat ../out/heard.out)
 	
 	if [[ $answer == 1 ]]; then 
-		echo -e "\nLe serveur a bien reçu la réponse"
+		echo -e "${Green}Le serveur a bien reçu la réponse${Green}"
 	else
-		echo -e "\nLe serveur n'est pas joignable ou n'est pas bien configuré, veuillez réessayer"
+		echo -e "${Red}Le serveur n'est pas joignable ou n'est pas bien configuré, veuillez réessayer${Red}"
 		exit
 	fi
 }
 
 function check_var {
 	if [[ -z $1 ]]; then 
-		echo -e "\nVeuillez donnez un nom"
+		echo -e "\n${Red}Veuillez donner un nom${Red}"
 		exit 
 	fi
 }
@@ -66,5 +69,8 @@ function wait {
 	sleep 2
 }
 
+Blue='\033[0;34m'
+Red='\033[0;31m'
+Green='\033[0;32m'
 service="start"
 main "$1"
--- script/test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-while IFS="," read -r rec_column1 rec_column2 rec_column3
-do
-  if [ $rec_column1 == $1 ]; then
-  	echo $rec_column1 
-  fi
-done < <(tail -n +2 ../csv/MonDns.csv)
